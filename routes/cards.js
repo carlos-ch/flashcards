f2dda1a2c9f86e07a973680f1dbc81571b38ef81
Glenn Atle Landenes <carlos.tschavez@gmail.com>
2020-08-01 22:39:38 +0200

    added more redirect

--- routes/cards.js
+++ routes/cards.js
@@ -2,15 +2,39 @@ const express = require('express');
 const router = express.Router()
 
 const { data } = require('../data/flashcardData.json');
-const { cards }= data;
+const { cards } = data;
+
+router.get('/', (req, res) => {
+  const cardId = Math.floor(Math.random() * cards.length);
+  res.redirect(`/cards/${cardId}`);
+})
 
 
 router.get('/:id', (req, res) => {
-  console.log(cards[req.params.id]);
-  res.render('card', {
-    prompt: cards[req.params.id].question,
-    hint: cards[req.params.id].hint
-  });
+  const { side } = req.query;
+  const { id } = req.params; //randomId
+
+  if ( !side ) {
+    return res.redirect(`/cards/${id}?side=question`);
+  }
+
+  const name = req.cookies.username;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+
+  const templateData = { id, text, name };
+
+  if (side === 'question') {
+    templateData.flip = 'answer';
+    templateData.backto = 'Answer';
+    templateData.hint = hint;
+  } else if (side === 'answer') {
+    templateData.flip = 'question';
+    templateData.backto = 'Question';
+  };
+
+
+  res.render('card', templateData);
 });
 
 module.exports = router;
